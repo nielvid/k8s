4401f0aa6be1a36f42d23ae35ac8b8d1e5e79962
Nielvid <56310246+nielvid@users.noreply.github.com>
2022-05-12 03:07:52 +0100

    added new version

--- k8s-node-server-test/index.mjs
+++ k8s-node-server-test/index.mjs
@@ -2,14 +2,19 @@ import express from 'express'
 import os from 'os'
 const app = express()
 
+app.get('/', (req, res) => {
+  const message = `I am responding to request from ${os.hostname}`
+  console.log(message)
+  res.send(message)
+})
 
-app.get('/', (req, res)=>{
-    const message = `I am responding to request from ${os.hostname}`
-    console.log(message)
-    res.send(message)
+app.get('/v2', (req, res) => {
+  const message = `This route was added in version 2.0.0. Responding from  ${os.hostname}`
+  console.log(message)
+  res.send(message)
 })
 
 const PORT = process.env.PORT || 3000
-app.listen(PORT, ()=>{
-    console.log(`Server running on port: ${PORT}`)
+app.listen(PORT, () => {
+  console.log(`Server running on port: ${PORT}`)
 })
